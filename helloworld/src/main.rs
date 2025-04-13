e9782efab1ecdb15956e9a5894fe2df92ea5a689
leandro.iglezias <leeiglezias@gmail.com>
2025-04-13 16:01:36 -0300

    Guessing Game in Rust!

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -1,6 +1,8 @@
-
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
 fn main()
 {
+    println!("FIRST RUST APP RUNNING....");
     //declare var String
     let mut hello_world = String::from("Hello World!");
 
@@ -11,4 +13,11 @@ fn main()
     println!("{}", hello_world);
     hello_world.push_str(" of Universe");
     println!("{}", hello_world);
+
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
