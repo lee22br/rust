b10982e4e1ae59f787c36e82034be8124b1291e6
leandro.iglezias <leeiglezias@gmail.com>
2025-04-13 22:43:55 -0300

    Guessing Game in Rust!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,6 +6,7 @@ use rand::Rng;
 
 fn main()
 {
+    //Start of playground
     let apples = 5; // immutable
     let mut bananas = 5; // mutable
     bananas += 5;
@@ -14,26 +15,22 @@ fn main()
     let x = 5;
     let y = 10;
     println!("x = {x} and y + 2 = {}", y + 2);
+    // end of playground
 
-
+    //mini-game Guess the number
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=10);
 
-    //println!("The secret number is: {secret_number}");
     loop {
         println!("Please input your guess.");
-
         let mut guess = String::new();
-
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-
         println!("You guessed: {guess}");
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
@@ -44,5 +41,5 @@ fn main()
             }
         }
     }
-
+    println!("Game over!");
 }
