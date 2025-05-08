c65d4d70fbe18870eff3b61924fb212ab2eb247c
lee22br <leeiglezias@gmail.com>
2025-05-08 07:43:56 -0300

    Exercism Gigasecond

--- gigasecond/src/main.rs
+++ gigasecond/src/main.rs
@@ -1,10 +1,13 @@
 use time::PrimitiveDateTime as DateTime;
+use time::Duration;
+
 fn main()
 {
-    println!("Hello, world!");
+    println!("Date after One billion seconds: {}", after(Date::now()));
 }
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    let billion = 1000000000;
+    start + Duration::seconds(billion)
 }
